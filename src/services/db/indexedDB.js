bcc17bcc8368e072a1223dea7575aa8df819ac06
iDevotion <omar60@live.com.mx>
2025-04-06 18:48:21 -0600

    feat: Creación de la configuración de conexión para IndexedDB

---
+++ src/services/db/indexedDB.js
@@ -0,0 +1,65 @@
+// Configuración para IndexedDB
+// v.0.1.0
+
+import { DB } from "../../constants/appConstants.js";
+
+let dbInstance = null;
+
+// Inicializamos la conexion a la base de datos
+export const inicializarDB = () => {
+  return new Promise((resolve, reject) => {
+    if (instanciaDB) {
+      resolve(dbInstance);
+      return;
+    }
+    const solicitud = indexedDB.open(DB.NAME, DB.VERSION);
+    solicitud.onupgradeneeded = (evento) => {
+      const db = evento.target.result;
+
+      if (!db.objectStoreNames.contains(DB.STORES.ACTIVIDADES)) {
+        const almacenActividades = db.createObjectStore(DB.STORES.ACTIVIDADES, {
+          keyPath: "id",
+          autoIncrement: true,
+        });
+        almacenActividades.createIndex(DB.INDEXES.FECHA, "fecha", {
+          unique: false,
+        });
+      }
+    };
+    solicitud.onsuccess = (evento) => {
+      instanciaDB = evento.target.result;
+      console.log("Base de datos inicializada:");
+      resolve(instanciaDB);
+    };
+    solicitud.onerror = (evento) => {
+      console.error(
+        "Error al inicializar la base de datos:",
+        evento.target.error
+      );
+      reject(evento.target.error);
+    };
+  });
+};
+
+// Obtener la instancia de la base de datos
+export const obtenerDB = async () => {
+  if (!instanciaDB) {
+    await inicializarDB();
+  }
+  return instanciaDB;
+};
+
+// Cerrar la conexion a la base de datos
+export const cerrarDB = () => {
+  if (instanciaDB) {
+    instanciaDB.close();
+    instanciaDB = null;
+    console.log("Base de datos cerrada.");
+  }
+};
+
+export default {
+  inicializarDB,
+  obtenerDB,
+  cerrarDB,
+};
